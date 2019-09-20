e1cf8851872e924eab70d8409e3a3411c9b94fac
Arnav Sarin <arnavsarin@arnavs-macbook-pro.local>
2019-09-20 18:51:03 -0500

    Initial Commit

---
+++ ChicagoCH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChicagoCH
+//
+//  Created by Arnav Sarin on 9/20/19.
+//  Copyright © 2019 Arnav Sarin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
