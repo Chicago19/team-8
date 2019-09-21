f7b820b18197f37f0f57748321d9252d0d8f9ded
aneeshmysore <aneeshmysore@icloud.com>
2019-09-20 21:14:56 -0500

    Finished adding homepage buttons

--- ChicagoCH/ViewController.swift
+++ ChicagoCH/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var kidsButton: UIButton!
+    @IBOutlet weak var parentButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        parentButton.backgroundColor = .clear
+        parentButton.layer.cornerRadius = 75
+        parentButton.layer.borderWidth = 1
+        parentButton.layer.borderColor = UIColor.black.cgColor
+        
+        kidsButton.backgroundColor = .clear
+        kidsButton.layer.cornerRadius = 75
+        kidsButton.layer.borderWidth = 1
+        kidsButton.layer.borderColor = UIColor.black.cgColor
+        
+        parentButton.titleLabel!.font = UIFont(name: "Times New Roman", size: 25)
+        
+        kidsButton.titleLabel!.font = UIFont(name: "Times New Roman", size: 25)
+        
+        //passLock.isHidden = true
+        
+        
+    }
+    
+    @IBAction func InsertPaswd(_ sender: Any) {
+        //passLock.isHidden = false
+        
+        //        parentButton.isHidden = true
+        //        kidsButton.isHidden = true
     }
 
 
